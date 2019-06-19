381a13b69b35fe1d5ffefaa4f0b7307bc6fa9219
atlee19 <atleegraham14@gmail.com>
2019-06-19 16:43:21 -0400

    Buttons are now semi functional - ready to perform actions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from control import ActionButtons
 
 
@@ -7,9 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '857c97a793ec5f9010971b2e0b4a68bf'
 
 
-@app.route('/panel')
+@app.route('/panel', methods=['GET','POST'])
 def LoadPanel():
 	form = ActionButtons()
+	if form.is_submitted():
+		if form.button00.data:
+			print('button00 pressed')
+		elif form.button01.data:
+			print('button01 pressed')
+		
+
 	return render_template("panel.html", form=form)
 
 
--- control.py
+++ control.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm 
 from wtforms import SubmitField
 
-
 class ActionButtons(FlaskForm):
 	button00 = SubmitField('Button00')
+	button01 = SubmitField('Button01')
 
 
