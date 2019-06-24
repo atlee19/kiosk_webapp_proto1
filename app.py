30c247f5219d36853f0d68a3e5e5f18f2a2d6161
atlee19 <atleegraham14@gmail.com>
2019-06-24 16:21:35 -0400

    Major css styling overhaul

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '857c97a793ec5f9010971b2e0b4a68bf'
 
 
-@app.route('/panel', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def LoadPanel():
 	form = ActionButtons()
 	if form.is_submitted():
 		if form.event01.data:
 			success = execute_js('play.js', ' -i 0.0.0.0 -e ./events/1.evt')
 			if success:
-				flash('Event 1 triggered')
+				flash('Event sent.')
 
 		elif form.event02.data:
 			print('event 2 triggered')
@@ -30,9 +30,9 @@ def LoadPanel():
 			print('event 5 triggered')
 			success = execute_js('play.js', ' -i 0.0.0.0 -e ./events/5.evt')
 		elif form.activate.data:
-			print('Server activated...')
+			flash('Server Activated')
 			success = execute_js('serve.js')
-
+	
 		elif form.deactivate.data:
 			print('Server deactivated...')
 		
